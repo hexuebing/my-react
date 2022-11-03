9da50d4ac4207b691ee39e0cdd4a9d8a04276bb5
Gift <gift@Gifts-MacBook-Pro.local>
2022-11-03 22:25:20 +0800

    处理判断节点的问题

--- src/MyReact/createElement.js
+++ src/MyReact/createElement.js
@@ -1,11 +1,14 @@
 export default function createElement (type, props, ...children){
-  const childElement = children.map(child => {
-    if(child instanceof Object){
-      return child
-    }else{
-      return createElement('text', { textContent: child})
+  const childElement = children.reduce((result, child) => {
+    if(child !== true && child !== false && child !== null){
+      if(child instanceof Object){
+        result.push(child)
+      }else{
+        result.push(createElement('text', { textContent: child}))
+      }
     }
-  })
+    return result
+  }, [])
   return {
     type, props, children: childElement
   }
