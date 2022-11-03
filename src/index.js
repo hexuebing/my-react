90b3ab279339e52788a4e38b32f1a86c5cd75284
Gift <gift@Gifts-MacBook-Pro.local>
2022-11-03 21:37:05 +0800

    使用vite启动

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import MyReact from './MyReact/index.js'
 const virtualDOM = (
   <div className="app">
     <h1>hello</h1>
+    <div>
+      嵌套第一层
+      <div>嵌套第二层</div>
+    </div>
+    {false && <div>不该出现的JSX</div>}
+    {true && <div>该出现的JSX</div>}
+    <button onClick={() => alert("👋")}>按钮</button>
   </div>
 )
 
-console.log(123)
+console.log(virtualDOM)
